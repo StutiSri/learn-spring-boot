6088429a2aecc885e4c2d1e6bf3eceb8d0f83b9a
Stuti Srivastava <stutisrivastv@gmail.com>
2018-05-17 23:27:50 +0530

    @ssrivastava - Integrates actuator with App

---
+++ api/src/main/java/com/learn/springboot/api/SpringSecurityConfig.java
@@ -0,0 +1,16 @@
+package com.learn.springboot.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password("admin").roles("ACTRADMIN");
+    }
+}
